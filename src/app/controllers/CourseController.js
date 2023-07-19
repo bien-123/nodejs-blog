70750e283b7fa9a06a130d5fc3511b40046502c1
bien-123 <bienle160720@gmail.com>
2023-07-20 02:57:06 +0700

    [CRUD] Create new course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,5 +1,3 @@
-// chứa những controller ở ngoài
-
 const Course = require('../models/Course');
 const { mutipleMongooseToObject, mongooseToObject } = require('../../util/mongoose');
 
@@ -11,6 +9,24 @@ class CourseController {
                 res.render('courses/show', { course: mongooseToObject(course) });
             })
             .catch(next);
+    }
+
+    // [GET] /course/create : tạo ra UI cho form tạo mới
+    create(req, res, next) {
+        res.render('courses/create');
+    }
+
+    // [POST] /course/store : thêm dữ liệu từ form tạo mới lên DB
+    store(req, res, next) {
+        // res.json(req.body.name);//dùng để login
+
+        const formData = req.body;
+        formData.image = `https://img.youtube.com/vi/${req.body.video}/sddefault.jpg`;
+        const course = new Course(formData);
+        course
+            .save()
+            .then(() => res.redirect('/')) //thêm xong sẽ chuyển hướng về trang
+            .catch((error) => {});
     }
 }
 
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,24 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
+mongoose.plugin(slug);
+
 const Schema = mongoose.Schema;
 
-const Course = new Schema({
-    name: { type: String, maxLength: 255 },
-    description: { type: String, maxLength: 600 },
-    image: { type: String, maxLength: 255 },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
-});
+// Schema: phải có field trong column ở table thì mới thêm đc
+const Course = new Schema(
+    {
+        name: { type: String, require: true },
+        description: { type: String, maxLength: 600 },
+        image: { type: String, maxLength: 255 },
+        video: { type: String, require: true },
+        level: { type: String, maxLength: 255 },
+        slug: { type: String, slug: 'name', unique: true }, //unique: true: chỉ có duy nhất 1 giá trị
+        // createdAt: { type: Date, default: Date.now }, //từ version 4. trở lên mongose tự tạo nếu thêm timestamps: true
+        // updatedAt: { type: Date, default: Date.now },
+    },
+    {
+        timestamps: true,
+    },
+);
 
 module.exports = mongoose.model('Course', Course);
